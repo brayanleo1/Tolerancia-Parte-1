080cd9fb97bdd291ccca7bbd10376e4bb6a6ed60
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2024-12-21 12:29:41 -0300

    two simulated request (need databank)

---
+++ Req1.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import random
+import time
+import uuid
+
+app = Flask(__name__)
+
+@app.route('/product', methods=['GET'])
+def request1():
+    # Simular falha de omissão com 20% de probabilidade
+    if random.random() < 0.2:
+        print("Request 1 (Store): Falha de omissão simulada.")
+    else:
+        data = request.get_json()
+        product_id = data["product_id"]
+
+        # Simular resposta bem-sucedida
+        product = {"id": product_id, "name": "Produto X", "value": 100.0}
+        print(f"Request 1 (Store): Sucesso - {product}")
+        return jsonify(product)
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ Req2.py
@@ -0,0 +1,24 @@
+# app.py
+from flask import Flask, request, jsonify
+import random
+import signal
+import os
+app = Flask(__name__)
+
+@app.route('/exchange', methods=['GET'])
+def request2():
+    # Simular falha de crash com 10% de probabilidade
+    if random.random() < 0.1:
+        print("Request 2 (Exchange): Falha de crash simulada. Serviço indisponível.")
+        # Forçar a interrupção do programa rodando
+        #raise RuntimeError("Simulated crash")
+        return os.kill(os.getpid(), signal.SIGINT)
+        
+
+    # Simular resposta bem-sucedida
+    exchange_rate = round(random.uniform(4.5, 5.5), 2)  # Exemplo de taxa de conversão
+    print(f"Request 2 (Exchange): Sucesso - Taxa de conversão: {exchange_rate}")
+    return jsonify({"exchange_rate": exchange_rate})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
